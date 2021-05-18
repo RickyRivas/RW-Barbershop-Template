ae390c923bc18196072bbbf0f38177899c1a7e65
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2021-05-17 20:51:45 -0500

    barbershop template start

---
+++ scripts/scripts.js
@@ -0,0 +1,39 @@
+const navSlide = () => {
+  const burger = document.querySelector(".hamburger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    //Toggle Nav
+    nav.classList.toggle("is-active");
+    //Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade .1s ease-in forwards ${
+          index / 7 + 0.3
+        }s`;
+      }
+    });
+    //burger animation
+    burger.classList.toggle("is-active");
+  });
+};
+navSlide();
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 0,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
